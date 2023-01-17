f7d25e2774ee25d38bdf79ff2ca80b9c54a2fa07
hiluan <apt.muenchen@gmail.com>
2023-01-16 17:07:38 -0800

    redux tool kit Query and injecting mock data into MongoDB

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports (use only ONCE)
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 // CONFIGURATION
 dotenv.config();
 const app = express();
@@ -36,5 +40,7 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // ONLY ADD THIS DATA ONE TIME FOR MOCK DB
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect.`));
